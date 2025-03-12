e7239e5c26478b4241ccce6c2ff1c8eba984c490
Moon1466 <ddtank20000009@gmail.com>
2025-03-12 18:22:29 +0700

    V0.6: Restore FE

---
+++ Front_End/src/main.jsx
@@ -0,0 +1,32 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ProductDetail from "./pages/ProductDetail/ProductDetail.jsx";
+import Cart from "./pages/Cart/Cart.jsx";
+import Payment from "./pages/Payment/Payment.jsx";
+import Search from "./pages/Search/Search.jsx";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/product-detail",
+    element: <ProductDetail />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+  {
+    path: "/payment",
+    element: <Payment />,
+  },
+  {
+    path: "/search",
+    element: <Search />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(<RouterProvider router={router} />);
