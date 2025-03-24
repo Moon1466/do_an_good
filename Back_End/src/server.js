c2c8018e8a5dc7661c39b1b4772eb6c69c6d114d
Moon1466 <ddtank20000009@gmail.com>
2025-03-24 11:05:47 +0700

    V0.8.5: Update View

---
+++ Back_End/src/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 3000
+
+// USE TE
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+// 
+
+app.get('/', (req, res) => {
+  res.render('index.ejs')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
