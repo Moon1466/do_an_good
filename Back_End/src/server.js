ab8ed16918985bf7180b7676d9e44e688bb9f7b4
Moon1466 <ddtank20000009@gmail.com>
2025-04-07 23:46:30 +0700

    V1.0: Connect Database and Update UI BE

--- Back_End/src/server.js
+++ Back_End/src/server.js
@@ -6,22 +6,33 @@ const app = express()
 const configViewEngine = require('./config/viewEngine')
 const webRoutes = require('./routes/web');
 const { config } = require('dotenv')
+const connection = require('./config/database')
+
+const User = require('./model/Users')
 
 const port = process.env.PORT || 3001
 const hostname = process.env.HOST_NAME || 'localhost'
 
-console.log(hostname)
 // config
 
 configViewEngine(app)
 
 // config route
 
-app.use('/' , webRoutes)
+app.use('/' , webRoutes);
 
-// 
+//  test
 
  
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port} `)
-})
+
+// Launch app
+(async () => {
+  try {
+    await connection();
+    app.listen(port, () => {
+      console.log(`Example app listening on port Apple Music `);
+    });
+  } catch (error) {
+    console.error("Error connecting to the database:", error);
+  }
+})();
